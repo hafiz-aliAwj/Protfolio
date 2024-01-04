2c10f943dc9963a77d46a3514a03082fc9434bf2
hafiz-aliAwj <hafizaliawj112@gmail.com>
2024-01-04 17:24:59 +0500

    hero section done

---
+++ my-app/src/components/hero/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+export default function Hero() {
+  return (
+    <div className="bg-[#040b14] text-[white] w-full h-screen flex flex-col items-start justify-center">
+      <div className="w-3/5 mx-auto">
+        <h1 className="text-4xl font-black ">Hafiz Ali Awj</h1>
+        <div className="flex justify-start items-baseline">
+          <pre className="text-sm">I'm a </pre>
+          <div className="inline text-lg pb-1 border-b-2 border-[#149ddd]">
+            <Typewriter
+              options={{
+                strings: [
+                  "MERN Stack Developer",
+                  "Front End Developer",
+                  "Freelancer",
+                  "Next JS Developer",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,9 +1,12 @@
+import Hero from "@/components/hero";
+import Navbar from "@/components/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1>HI</h1>
+      <Hero />
+      <Navbar />
     </>
   );
 }

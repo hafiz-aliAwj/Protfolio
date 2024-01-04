365a2ab5873cddfff773c7fd442731d49d10f735
hafiz-aliAwj <hafizaliawj112@gmail.com>
2024-01-04 17:28:42 +0500

    Update index.js

--- my-app/src/components/hero/index.js
+++ my-app/src/components/hero/index.js
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div className="bg-[#040b14] text-[white] w-full h-screen flex flex-col items-start justify-center">
       <div className="w-3/4 mx-auto">
-        <h1 className="text-4xl font-black ">Hafiz Ali Awj</h1>
+        <h1 className="text-4xl font-black ">HAFIZ ALI AWJ</h1>
         <div className="flex justify-start items-baseline">
           <pre className="text-sm">I'm a </pre>
           <div className="inline text-lg pb-1 border-b-2 border-[#149ddd]">

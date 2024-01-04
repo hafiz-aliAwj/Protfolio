6b02e057ee92bfc71c5da3f69c328f4a6722d2b4
hafiz-aliAwj <hafizaliawj112@gmail.com>
2024-01-04 19:54:56 +0500

    created about

--- my-app/src/components/hero/index.js
+++ my-app/src/components/hero/index.js
@@ -4,11 +4,11 @@ import Typewriter from "typewriter-effect";
 export default function Hero() {
   return (
     <div className="bg-[#040b14] text-[white] w-full h-screen flex flex-col items-start justify-center">
-      <div className="w-3/4 mx-auto">
-        <h1 className="text-4xl font-black ">HAFIZ ALI AWJ</h1>
+      <div className="w-4/5 mx-auto">
+        <h1 className="text-6xl font-black ">ALI AWJ</h1>
         <div className="flex justify-start items-baseline">
-          <pre className="text-sm">I'm a </pre>
-          <div className="inline text-lg pb-1 border-b-2 border-[#149ddd]">
+          <pre className="text-sm font-medium">I'm a </pre>
+          <div className="inline text-xl font-medium pb-1 border-b-2 border-[#149ddd]">
             <Typewriter
               options={{
                 strings: [

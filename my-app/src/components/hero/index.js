0018aea6fa22fe9d09dcd5166ad64d8a8112eb95
hafiz-aliAwj <hafizaliawj112@gmail.com>
2024-01-24 02:25:28 +0500

    started

--- my-app/src/components/hero/index.js
+++ my-app/src/components/hero/index.js
@@ -3,11 +3,11 @@ import Typewriter from "typewriter-effect";
 
 export default function Hero() {
   return (
-    <div className="bg-[#040b14] text-[white] w-full h-screen flex flex-col items-start justify-center">
-      <div className="w-4/5 mx-auto">
+    <div className="bg-[#040b14] text-[white] w-full  h-screen flex flex-col items-start justify-center">
+      <div className="w-4/5  mx-auto">
         <h1 className="text-6xl font-black ">ALI AWJ</h1>
         <div className="flex justify-start items-baseline">
-          <pre className="text-sm font-medium">I'm a </pre>
+          <pre className="text-sm font-raleway font-medium">I'm a </pre>
           <div className="inline text-xl font-medium pb-1 border-b-2 border-[#149ddd]">
             <Typewriter
               options={{
--- my-app/src/components/navbar/index.js
+++ my-app/src/components/navbar/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
-    return (
-      <>
-        <h1>HI</h1>
-      </>
-    );
-  }
+  return <div>
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>;
+}
